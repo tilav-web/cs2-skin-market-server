276241aac8b4e0af90806eaae16a87989b766f2b
tilav-web <shavqiddintilovov0@gmail.com>
2025-07-18 02:28:54 +0500

    .

--- src/modules/telegram-publisher/telegram-publisher.processor.ts
+++ src/modules/telegram-publisher/telegram-publisher.processor.ts
@@ -72,7 +72,7 @@ export class TelegramPublisherProcessor extends WorkerHost {
                         ? 'TEKINGA OLING😊'
                         : `${skin.price} so'm`,
                     web_app: {
-                      url: `https://cs2.cashout.uz/skins/buy/${data.skinId}`,
+                      url: `${this.telegramBotUrl}/WebApp?startapp=skins_buy_${data.skinId}`,
                     },
                   },
                 ],
